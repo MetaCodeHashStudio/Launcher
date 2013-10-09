522a4a48d281953ba8137d1c3e2acc4b083b8c1d
Runnetty1 <rcdriftnitro2@gmail.com>
2013-10-09 09:18:02 +0200

    Install readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Launcher
 ========
 
 Yahtzee Launcher
+
+1. Create a new database with the name: yahtzoid_acc.
+2. Run f.sql file.
+3. Start coding. :p
---
+++ f.sql
@@ -0,0 +1,44 @@
+
+SELECT aNickname,aPassword,aEmail,aGender,aDate,aLvl,aGold,aXP FROM metaAcc,yahtzoid
+ORDER BY aNickname,aPassword,aEmail,aGender,aDate,aLvl,aGold,aXP DESC;
+
+SELECT AES_DECRYPT('good','kk') FROM metaAcc;
+
+
+SELECT * FROM metaAcc;
+SELECT * FROM yahtzoid;
+
+CREATE TABLE metaAcc
+(
+accID int(1) not null unique AUTO_INCREMENT,
+aNickname char(15) not null unique,
+aPassword varchar(255) not null,
+aEmail char(50) not null unique,
+aGender char(6) not null,
+aDate char(12)not null,
+PRIMARY KEY(accID)
+);
+
+
+CREATE TABLE yahtzoid
+(
+ID int(1) not null unique AUTO_INCREMENT,
+aLvl int not null,
+aGold int,
+aXP int,
+Foreign key (ID) REFERENCES metaAcc (accID)
+);
+
+INSERT INTO metaacc (aNickname, aPassword, aEmail, aGender,aDate)
+value('Runnetty2', DES_ENCRYPT('good','kk'),'mats19922@gmail.com', 'male', '10.12.92');
+
+INSERT INTO metaAcc_Details (aEmail, aGender,aDate)
+VALUE('mats1992@gmail.com', 'male', '10.12.92');
+
+INSERT INTO yahtzoid (aLvl, aGold,aXP)
+VALUE(1,1000,0);
+
+DROP table yahtzoid,metaAcc;
+
+DROP table metaAcc;
+DROP table yahtzoid;
